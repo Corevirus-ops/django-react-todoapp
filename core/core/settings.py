6af289b79ae428073c6faa5c5a0b8cb75323caf2
corevirus-ops <corevirus1738@gmail.com>
2026-09-23 00:47:37 -0700

    moved app into project lol

--- core/core/settings.py
+++ core/core/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-o#(1ttp&)7b5em^x&aysox3ez_4e6rzqo7p-p_)k==1j3=^r05
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '127.0.0.1']
 
 
 # Application definition
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'todo',
 ]
 
 MIDDLEWARE = [
